d829e44b11dc5c94bc67c5683b89689832885df2
Maksym <maksym@example.com>
2025-07-10 11:15:17 +0300

    feat: додано Program.cs

---
+++ PrecticGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Введіть c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введіть d: ");
+        double d = Convert.ToDouble(Console.ReadLine());
+        double result = -2 * c - d + 53;
+        Console.WriteLine($"Результат: {result}");
+
+        Console.ResetColor();
+    }
+}
